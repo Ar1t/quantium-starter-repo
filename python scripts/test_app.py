16cbc9fa371a2d69c82bcebd1ed541c3533b5422
Ar1t <zoydlife@gmail.com>
2026-05-17 14:36:53 +0100

    added test script

---
+++ python scripts/test_app.py
@@ -0,0 +1,17 @@
+from app import app
+
+
+def test_header_is_present(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("h1", timeout=10)
+    assert dash_duo.find_element("h1").text == "Pink Morsels Sales"
+
+
+def test_visualisation_is_present(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#sales-graph", timeout=10)
+
+
+def test_region_picker_is_present(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#region-filter", timeout=10)
